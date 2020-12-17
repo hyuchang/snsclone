95d156eea053130f90af9c65efdfe2c5b9150e75
huttchang <huttchang@gmail.com>
2020-12-17 23:30:49 +0900

    change Controller name

--- src/main/java/com/huttchang/sns/user/controller/UserController.java
+++ src/main/java/com/huttchang/sns/user/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("users")
 @RestController
 @RequiredArgsConstructor
-public class WelcomeController {
+public class UserController {
 
     private final UserService userService;
 
