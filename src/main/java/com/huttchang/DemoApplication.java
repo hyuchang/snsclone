a76c66d6f1728edd8bd78bd781c8993cf8acf159
huttchang <huttchang@gmail.com>
2021-01-08 16:12:06 +0900

    Springboot Main class의 위치를 변경함 컴포넌트 스캔의 기본위치가 메인클래스의 위치부터 하위로 잡히기 때문입니다.

--- src/main/java/com/huttchang/DemoApplication.java
+++ src/main/java/com/huttchang/DemoApplication.java
@@ -1,14 +1,12 @@
-package com.huttchang.sns;
+package com.huttchang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-@ComponentScan(value = {"com.huttchang.*"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
